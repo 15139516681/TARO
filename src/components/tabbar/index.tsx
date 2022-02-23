52d48eafe031c01f865efb14e63ca77a2383e46e
张亚辉 <15139516681@163.com>
2022-02-23 17:17:44 +0800

    fix: 修复eslint报错

--- src/components/tabbar/index.tsx
+++ src/components/tabbar/index.tsx
@@ -1,68 +1,72 @@
-import { FC } from 'react';
-import { Image, View } from '@tarojs/components';
-import Taro from "@tarojs/taro";
+import { FC } from "react"
+import { Image, View } from "@tarojs/components"
+import Taro from "@tarojs/taro"
 
-import './index.scss'
+import "./index.scss"
 
-
-const Tabbar: FC<{active:string}> = ({active}) => {
-
-
-    const toTab = (data:string) => {
-        switch (data){
-            case '首页':
-                Taro.redirectTo({url:'pages/index/index'})
-                break;
-            case '数据':
-                Taro.redirectTo({url:'pages/data/data'})
-                break;
-            case '订单':
-                Taro.redirectTo({url:'pages/order/order'})
-                break;
-            case '我的':
-                Taro.redirectTo({url:'pages/user/user'})
-                break;
-        }
-
+const Tabbar: FC<{ active: string }> = ({ active }) => {
+  const toTab = (data: string) => {
+    switch (data) {
+      case "首页":
+        Taro.redirectTo({ url: "pages/index/index" })
+        break
+      case "数据":
+        Taro.redirectTo({ url: "pages/data/data" })
+        break
+      case "订单":
+        Taro.redirectTo({ url: "pages/order/order" })
+        break
+      case "我的":
+        Taro.redirectTo({ url: "pages/user/user" })
+        break
     }
+  }
 
-    // tabbar组件
-    const tabbarBox = () => {
-        const data = [
-            {
-                title: '首页',
-                icon: require('@/assets/image/tabbar/home.png'),
-                iconA: require('@/assets/image/tabbar/homeA.png')
-            },
-            {
-                title: '数据',
-                icon: require('@/assets/image/tabbar/data.png'),
-                iconA: require('@/assets/image/tabbar/dataA.png'),
-            },
-            {
-                title: '订单',
-                icon: require('@/assets/image/tabbar/order.png'),
-                iconA: require('@/assets/image/tabbar/orderA.png'),
+  // tabbar组件
+  const tabbarBox = () => {
+    const data = [
+      {
+        title: "首页",
+        icon: require("@/assets/image/tabbar/home.png"),
+        iconA: require("@/assets/image/tabbar/homeA.png"),
+      },
+      {
+        title: "数据",
+        icon: require("@/assets/image/tabbar/data.png"),
+        iconA: require("@/assets/image/tabbar/dataA.png"),
+      },
+      {
+        title: "订单",
+        icon: require("@/assets/image/tabbar/order.png"),
+        iconA: require("@/assets/image/tabbar/orderA.png"),
+      },
+      {
+        title: "我的",
+        icon: require("@/assets/image/tabbar/user.png"),
+        iconA: require("@/assets/image/tabbar/userA.png"),
+      },
+    ]
+    return data.map((item: { title: string; icon: any; iconA: any }) => {
+      return (
+        <View
+          className="tabbar_single_box"
+          onClick={() => {
+            toTab(item.title)
+          }}
+          key={item.title}
+        >
+          <View className="tabbar_single_image">
+            <Image
+              className="img"
+              src={active === item.title ? item.iconA : item.icon}
+            />
+          </View>
+          <View className="tabbar_single_name">{item.title}</View>
+        </View>
+      )
+    })
+  }
 
-            },
-            {
-                title: '我的',
-                icon: require('@/assets/image/tabbar/user.png'),
-                iconA: require('@/assets/image/tabbar/userA.png'),
-            },
-        ]
-        return data.map((item: { title: string; icon: any,iconA:any }) => {
-            return (
-                <View className='tabbar_single_box' onClick={()=>{toTab(item.title)}} key={item.title}>
-                    <View className='tabbar_single_image'>
-                        <Image className='img' src={active === item.title ? item.iconA: item.icon} />
-                    </View>
-                    <View className='tabbar_single_name'>{item.title}</View>
-                </View>
-            )
-        })
-    }
-
-    return <View className='tabbar'>{tabbarBox()}</View>
+  return <View className="tabbar">{tabbarBox()}</View>
 }
 export default Tabbar
--- src/pages/data/board.tsx
+++ src/pages/data/board.tsx
@@ -1,40 +1,45 @@
-import  {FC} from 'react';
-import { Text, View } from '@tarojs/components'
+import { FC } from "react"
+import { Text, View } from "@tarojs/components"
 
-const Board:FC = () => {
-
-
-    return(
-        <View className='board'>
-            <View className='board_box'>
-            <View className='board_top'>
-                <View className='board_top_left'>
-                <View className='board_title'>支付客户</View>
-                <View className='board_num'>116</View>
-                <View className='yesterday'><Text className='yesterday_name'>昨日</Text>98</View>
-                </View>
-                <View className='board_top_right'>
-                    <View className='board_title'>新增客户</View>
-                    <View className='board_num'>122</View>
-                    <View className='yesterday'><Text className='yesterday_name'>昨日</Text>98</View>
-                </View>
+const Board: FC = () => {
+  return (
+    <View className="board">
+      <View className="board_box">
+        <View className="board_top">
+          <View className="board_top_left">
+            <View className="board_title">支付客户</View>
+            <View className="board_num">116</View>
+            <View className="yesterday">
+              <Text className="yesterday_name">昨日</Text>98
             </View>
-                <View className='board_bottom'>
-                    <View className='board_top_left'>
-                        <View className='board_title'>支付客户</View>
-                        <View className='board_num'>116</View>
-                        <View className='yesterday'><Text className='yesterday_name'>昨日</Text>98</View>
-                    </View>
-                    <View className='board_top_right'>
-                        <View className='board_title'>新增客户</View>
-                        <View className='board_num'>122</View>
-                        <View className='yesterday'><Text className='yesterday_name'>昨日</Text>98</View>
-                    </View>
-                </View>
+          </View>
+          <View className="board_top_right">
+            <View className="board_title">新增客户</View>
+            <View className="board_num">122</View>
+            <View className="yesterday">
+              <Text className="yesterday_name">昨日</Text>98
             </View>
-
+          </View>
         </View>
-    )
+        <View className="board_bottom">
+          <View className="board_top_left">
+            <View className="board_title">支付客户</View>
+            <View className="board_num">116</View>
+            <View className="yesterday">
+              <Text className="yesterday_name">昨日</Text>98
+            </View>
+          </View>
+          <View className="board_top_right">
+            <View className="board_title">新增客户</View>
+            <View className="board_num">122</View>
+            <View className="yesterday">
+              <Text className="yesterday_name">昨日</Text>98
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  )
 }
 
-export  default  Board;
+export default Board
